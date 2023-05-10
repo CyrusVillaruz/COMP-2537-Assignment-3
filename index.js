016e8e7611f36541bc8548e91405668287c02105
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-05-10 15:27:34 -0700

    Commit 3.2 - Implements pokemon details display logic

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const setup = async () => {
   let response = await axios.get(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=810"
   );
-  
+
   const pokemons = response.data.results;
   pokemons.forEach(async (pokemon) => {
     const res = await axios.get(pokemon.url);
     $("#pokeCards").append(`
-    <div class="pokeCard card">
+    <div class="pokeCard card" pokeName=${res.data.name}>
     <h3>${res.data.name.toUpperCase()}</h3> 
     <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
@@ -16,5 +16,44 @@ const setup = async () => {
     </button>
     </div> 
     `);
+  });
+
+  $("body").on("click", ".pokeCard", async function (e) {
+    const pokemonName = $(this).attr("pokeName");
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const types = res.data.types.map((type) => type.type.name);
+    $(".modal-body").html(`
+        <div style="width:200px">
+        <img src="${
+          res.data.sprites.other["official-artwork"].front_default
+        }" alt="${res.data.name}"/>
+        <div>
+        <h3>Abilities</h3>
+        <ul>
+        ${res.data.abilities
+          .map((ability) => `<li>${ability.ability.name}</li>`)
+          .join("")}
+        </ul>
+        </div>
+        <div>
+        <h3>Stats</h3>
+        <ul>
+        ${res.data.stats
+          .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
+          .join("")}
+        </ul>
+        </div>
+        </div>
+          <h3>Types</h3>
+          <ul>
+          ${types.map((type) => `<li>${type}</li>`).join("")}
+          </ul>
+      
+        `);
+    $(".modal-title").html(`
+        <h2>${res.data.name.toUpperCase()}</h2>
+        `);
   });
 };
